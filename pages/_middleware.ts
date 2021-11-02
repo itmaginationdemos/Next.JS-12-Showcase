18ffdbb0f9d1c21a37f28a6e458b4d443b678db0
Aleksander <aleksander.jess@itmagination.com>
2021-11-02 10:09:26 +0100

    adding some examples of the new Edge Runtime functionality

---
+++ pages/_middleware.ts
@@ -0,0 +1,25 @@
+import type { NextRequest } from 'next/server';
+
+
+// **Not all** functions are available from the Edge Runtime 
+// For a list of available APIs, see https://nextjs.org/docs/api-reference/edge-runtime
+export function middleware(req: NextRequest) {
+    // just two of the ways you can set custom headers.
+    // There are more, as well
+    // https://developer.mozilla.org/en-US/docs/Web/API/Headers
+    const headers = new Headers({ 'x-foo': 'bar' });
+    headers.set('x-fizz', 'buzz');
+
+    // We can print to the console from here as well.
+    console.time("Lorem Ipsum");
+
+    const country = req.geo.country;
+    const city = req.geo.city;
+    // sorry San Marino; but congrats on scoring against Poland!
+    const BLOCKED_COUNTRY = "SM"
+
+    if (country === BLOCKED_COUNTRY) {
+        return new Response('Sorry, San Marino', { status: 451 })
+    }
+    return new Response(`Hello, ${city},${country}!`)
+}
