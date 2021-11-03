87700f8e5b7c33e719988a130b1d71d05d59b69b
Aleksander <aleksander.jess@itmagination.com>
2021-11-03 15:30:23 +0100

    had to replace a dynamic url import with a static one. Dynamic url imports seem to not work currently when deploying the app to vercel.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-// URL imports
-import catNames from 'https://cdn.skypack.dev/cat-names';
+// URL imports. Should work, but they don't so, we had to substitute them for a static package
+import { random } from 'cat-names';
 
 const Home: NextPage = () => {
   const [catName, setCatName] = useState("")
 
   useEffect(() => {
-    setCatName(catNames.random())
+    setCatName(random())
 
   }, [])
 
   function onGetCatName() {
-    setCatName(catNames.random());
+    setCatName(random());
   }
 
   return (
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to the showcase of <a href="https://nextjs.org">Next.js 12!</a>
         </h1>
-        <p id="cat-name" style={{marginTop: "2rem"}}>Need a name for your new cat? Here you go: {catName}</p>
+        <p id="cat-name" style={{ marginTop: "2rem" }}>Need a name for your new cat? Here you go: {catName}</p>
         <p>Don't like it?</p>
         <button onClick={onGetCatName}>Get a new name</button>
       </main>
